5f49b0c9a7f6c2fdec8017bb23ccd09062cd2926
chris <jimenezchristopher5@gmail.com>
2024-05-25 11:59:40 -0500

    completed functionality and styling of reusable component feature

--- src/components/InventoryList/InventoryList.jsx
+++ src/components/InventoryList/InventoryList.jsx
@@ -1,15 +1,22 @@
+import "./InventoryList.css";
 
-
-const InventoryList = ({inventory}) => {
+//InventoryList Component
+const InventoryList = (props) => {
     return (
-      <div>
-        <h2>Inventory List</h2>
+      <div className="inventory-list">
+        <h2>{props.title}</h2>
         <ul>
-            {inventory.map((item)=>{
+            {props.inventory.map((item)=>{
                 return (
                     <li key={item._id}>
                         <p>{item.name}</p>
                         <p>Price: ${item.price}</p>
+
+                        {props.handleAddItem ? 
+                        <button onClick={()=> props.handleAddItem(item)}>Add Item</button> 
+                        : 
+                        <button onClick={()=> props.handleRemoveItem(item)}>Remove Item</button>}
+
                     </li>
                 )
             })}
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,19 +1,39 @@
 import {useState} from "react";
 import { inventoryData } from "../../data/data";
 import InventoryList from "../InventoryList/InventoryList";
+import "./Shop.css";
 
 
 //Child Component
 const Shop = () => {
 
     //State
-    const [inventory, setInventory] = useState(inventoryData);
+    const [shopInventory, setShopInventory] = useState(inventoryData);
+    const [userInventory, setUserInventory] = useState([]);
+
+    //Functions
+    const handleAddItem = (item) => {
+ 
+      //When click event happens, this event handler function has the state setter function - which it updates the state variable to an empty array
+      setUserInventory([...userInventory, item]);
+      setShopInventory(shopInventory.filter((element)=>{return element._id !== item._id}))
+    };
+
+    const handleRemoveItem = (item) => {
+      setShopInventory([...shopInventory, item]);
+      setUserInventory(userInventory.filter((element)=>{return element._id !== item._id}));
+    }
 
     return (
 
       <main>
         <h1>Shop</h1>
-        <InventoryList inventory={inventory}/>
+        {/* <button onClick={handleAddItem}>Click Here</button> */}
+        {/* We can create multiple instances of the same component, but pass DIFFERENT data to them to handle different features - We MUST give a title prop and then pass the unique state data to each instance of the component */}
+        <section className="shop-section">
+          <InventoryList title="Shop Inventory" inventory={shopInventory} handleAddItem={handleAddItem}/>
+          <InventoryList title="User Inventory" inventory={userInventory} handleRemoveItem={handleRemoveItem}/>
+        </section>
       </main>
 
     );
